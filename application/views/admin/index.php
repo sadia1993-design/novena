da7c38aafa5132c2cdebcc43193f5b37dfe004ec
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2021-11-30 16:42:15 +0600

    admin added

---
+++ application/views/admin/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() ?>admin_assets/css/style.css">
+
+    <title>Login Form</title>
+    <link rel="shortcut icon" href="<?php echo base_url() ?>admin_assets/images/favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+    <div class="container">
+
+        <div class="form-title text-center mb-5">
+            <h1>Login Form</h1>
+        </div>
+        <form method="post" action="" class="mb-5">
+            <label for="user" class="fw-bold mb-3">Enter Email</label>
+            <input class="form-control random mb-4 btn-lg" type="email" name="user" id="user" placeholder="E-mail Address" required>
+            <label for="password" class="fw-bold mb-3">Enter Password</label>
+            <input class="form-control mb-4 btn-lg" type="password" id="password" name="passwd" placeholder="Password" required>
+            <div class="form-button text-center">
+                <button type="submit" name="submit" class="btn btn-navy submit  " id="submit">Login</button>
+            </div>
+        </form>
+
+
+
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+
+</html>
