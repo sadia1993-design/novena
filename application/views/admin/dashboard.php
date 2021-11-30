61b62892bb52fa6b9667330c28b47f9f5ae11a0d
Nishat Sanjida <56416902+sadia1993-design@users.noreply.github.com>
2021-12-01 01:10:05 +0600

    footer edited

--- application/views/admin/dashboard.php
+++ application/views/admin/dashboard.php
@@ -5,7 +5,7 @@
         <div class="content-wrapper">
           <div class="row">
             <div class="col-sm-12">
-                hello world
+                
             </div>
           </div>
       </div>
--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,7 +1,7 @@
 <!-- partial:partials/_footer.html -->
 <footer class="footer">
   <div class="d-sm-flex justify-content-center justify-content-sm-between">
-    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Designed By <a href="" target="_blank">Nishat Sanjida</a> </span>
+    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block"><a href="" target="_blank" class="text-white text-decoration-none">Health Care Solutions</a> </span>
     <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Copyright © 2021. All rights reserved.</span>
   </div>
 </footer>
